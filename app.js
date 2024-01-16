ddbbbb5151ce2df9295ca78a9895cf5ba21b8e83
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2024-01-16 00:58:27 -0800

    feat: add dark mode with toggle

--- app.js
+++ app.js
@@ -73,3 +73,26 @@ document.addEventListener("keypress", function (event) {
     playSound(event.key);
     animateButton(event.key);
 });
+// Dark mode toggle
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+const enableDarkMode = () => {
+    document.body.classList.add("dark");
+    localStorage.setItem("darkMode", "enabled");
+};
+const disableDarkMode = () => {
+    document.body.classList.remove("dark");
+    localStorage.setItem("darkMode", null);
+};
+if (darkMode === "enabled") {
+    enableDarkMode();
+}
+darkModeToggle === null || darkModeToggle === void 0 ? void 0 : darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    }
+    else {
+        disableDarkMode();
+    }
+});
--- app.ts
+++ app.ts
@@ -87,3 +87,31 @@ document.addEventListener("keypress",
     animateButton(event.key);
 })
 
+// Dark mode toggle
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark");
+  localStorage.setItem("darkMode", null);
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle?.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+})
+
